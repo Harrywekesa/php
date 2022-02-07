91fefc06bc59ea7d4acc12a770fa49e34af75d44
Harrywekesa <harrisonwekesa09@gmail.com>
2022-02-07 15:18:08 +0300

    more

---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+$num1 = 10;
+$num2 = 30.665;
+$name = "Harrison Wekesa";
+$age = 23;
+$cars = array("volvo", "Toyota", "Mercedes", "Mobius");
+
+echo "This is an integer $num1";
+echo "<br>This is a float: $num2";
+echo "<br>My name is $name. I am $age years old";
+echo "<br>This is an array $cars";
+
+echo "<br>";
+var_dump($name);
+echo "<br>";
+var_dump($num2);
+echo "<br>";
+var_dump($num1);
+echo "<br>";
+var_dump($cars);
+echo"<br>";
+
+echo $num1 + $num2;
+echo "<br>";
+echo $num2/$num1;
+
+
