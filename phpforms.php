437f5071a09821498ef7bc207c01531184c5864f
Harrywekesa <harrisonwekesa09@gmail.com>
2022-02-07 18:50:27 +0300

    more

---
+++ phpforms.php
@@ -0,0 +1,22 @@
+<<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>details</title>
+</head>
+<body>
+<form action="submit.php" method="get">
+    <label for="fname">Full Name</label><br>
+    <input type="text" id="fname" name="name"><br>
+    <label for="email">Email Address</label><br>
+    <input type="email" id="eemail" name="email"><br>
+    <label for="gender">Gender</label><br>
+    <input type="text" id="gen" name="gender"><br><br>
+    <input type="submit" value="Register">
+</form>
+
+</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>submit</title>
+</head>
+<body>
+    <p>My name is <?php echo $_GET['name']?></p>
+    <p>My email address is <?php echo $_GET['email']?></p>
+    <p>My gender is <?php echo $_GET['gender']?>
+</body>
+</html>
